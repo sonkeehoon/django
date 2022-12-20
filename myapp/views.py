593a665a264dec6765cf2e5dd092ed80e072ab1f
kee_hoon <djfkfk12345@naver.com>
2022-12-20 18:32:36 +0900

    221220

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,91 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+nextId = 4
+topics = [
+    {'id':1, 'title':'routing', 'body':'Routing is ..'},
+    {'id':2, 'title':'view', 'body':'View is ..'},
+    {'id':3, 'title':'Model', 'body':'Model is ..'}
+]
+
+def HTMLTemplate(articleTag, id = None): # id값이 없는 경우 기본값 None
+    global topics
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+            <li>
+                <form action="/delete/" method="post">
+                    <input type="hidden" name="id" value={id}>
+                    <input type="submit" value="delete">
+                </form>
+            </li>
+        '''
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ul>
+            {ol}
+        </ul>
+        {articleTag}
+        <ul>
+            <li><a href="/create/">create</a></li>
+            {contextUI}
+        </ul>
+    </body>
+    </html>             
+    '''
+    
 
 def index(request):
-    return HttpResponse('Welcome!')
-
-def create(request):
-    return HttpResponse("Create!")
+    article = '''
+    <h2>Welcome</h2>
+    Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 def read(request, id):
-    return HttpResponse("Read!" + id)
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article, id))
+
+@csrf_exempt
+def create(request):
+    global nextId
+    if request.method == 'GET':
+        article = '''
+            <form action="/create/" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        newTopic = {"id":nextId,"title":title, "body":body}
+        topics.append(newTopic)
+        url = '/read/' + str(nextId)
+        nextId += 1
+        return redirect(url)
+
+@csrf_exempt
+
+def delete(request):
+    global topics
+    
+    if request.method == 'POST':
+        id = request.POST['id']
+        newTopics = []
+        for topic in topics:
+            if topic['id'] != int(id):
+                newTopics.append(topic)
+        topics = newTopics
+        return redirect('/')
